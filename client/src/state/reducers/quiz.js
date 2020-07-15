fddfeebb3f007a4edbcaca51bb288db427f95cd5
anasansari157 <anasansari157@gmail.com>
2020-07-15 12:55:31 +0530

    Add ATTEMPT_QUIZ Reducer

--- client/src/state/reducers/quiz.js
+++ client/src/state/reducers/quiz.js
@@ -3,41 +3,49 @@ import { quizList } from "../actions/quizActions";
 var intialState = {
   quizList: [],
   quiz: "",
+  result: "",
 };
 
 function Quiz(state = intialState, action) {
   switch (action.type) {
-    case "FETCH_QUIZ_LIST":
-      return { ...state, quizList: action.payload };
-    case "FETCH_QUIZ":
-      return { ...state, quiz: action.payload };
-    case "DELETE_QUIZ":
-      console.log(state.quizList);
-      return {
-        ...state,
-        quizList: {
-          ...state.quizList,
-          quizzes: state.quizList.quizzes.filter(
-            (quiz) => quiz._id != action.payload
-          ),
-        },
+		case "FETCH_QUIZ_LIST":
+			return { ...state, quizList: action.payload };
+		case "FETCH_QUIZ":
+			return { ...state, quiz: action.payload };
+		case "DELETE_QUIZ":
+			console.log(state.quizList);
+			return {
+				...state,
+				quizList: {
+					...state.quizList,
+					quizzes: state.quizList.quizzes.filter(
+						(quiz) => quiz._id != action.payload
+					),
+				},
+			};
+		case "ADD_NEW_QUIZ_QUESTION":
+			return {
+				...state,
+				quizList: {
+					...state.quizList,
+					quizzes: state.quizList.quizzes.map((quiz) => {
+						if (quiz._id == action.payload._id) {
+							return quiz.quesions.push(action.payload);
+						}
+						return quiz;
+					}),
+				},
+			};
+
+		case "ATTEMPT_QUIZ":
+			return {
+				...state,
+				result: action.payload,
       };
-    case "ADD_NEW_QUIZ_QUESTION":
-      return {
-        ...state,
-        quizList: {
-          ...state.quizList,
-          quizzes: state.quizList.quizzes.map((quiz) => {
-            if (quiz._id == action.payload._id) {
-              return quiz.quesions.push(action.payload);
-            }
-            return quiz;
-          }),
-        },
-      };
-    default:
-      return state;
-  }
+    
+		default:
+			return state;
+	}
 }
 
 export default Quiz;
