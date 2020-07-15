a43b29a8af672b48e34b6d029236923568883ed7
anasansari157 <anasansari157@gmail.com>
2020-07-15 12:54:09 +0530

    Add attempted Result in state and dispatch

--- client/src/views/quiz/attemptQuiz.js
+++ client/src/views/quiz/attemptQuiz.js
@@ -1,47 +1,97 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { showQuiz } from "../../state/actions/quizActions";
+import { attemptQuiz } from "../../state/actions/quizActions";
 
 class AttemptQuiz extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			questions: [],
+		};
 	}
 	componentDidMount() {
-		console.log(this.props.match.params.id);
-		this.props.dispatch(showQuiz(this.props.match.params.id));
+		var quizId = this.props.match.params.id;
+		this.props.dispatch(showQuiz(quizId));
 	}
+	componentDidUpdate(prevProps) {
+		if (prevProps !== this.props) {
+			var allQuestions = this.props.quiz.questions.map((question) => {
+				var obj = { questionId: question._id, answers: [] };
+				return obj;
+			});
+			this.setState({ questions: allQuestions });
+		}
+	}
+	handleCheckbox = (e, option, quesId, index) => {
+		var newQuestions = this.state.questions;
+		if (e.target.checked) {
+			newQuestions = newQuestions.map((question, i) => {
+				if (i === index && !question.answers.includes(option)) {
+					question.answers.push(option);
+					return question;
+				}
+				return question;
+			});
+			this.setState({ questions: newQuestions });
+		} else {
+			newQuestions = newQuestions.map((question, i) => {
+				if (i === index && question.answers.includes(option)) {
+					question.answers = question.answers.filter(
+						(answer) => answer !== option
+					);
+					return question;
+				}
+				return question;
+			});
+			this.setState({ questions: newQuestions });
+		}
+	};
+
+	handleSubmitQuiz = () => {
+		var attempt;
+		attempt = {
+			quizId: this.props.match.params.id,
+			questions: this.state.questions,
+		};
+		this.props.dispatch(attemptQuiz({ attempt }));
+		this.props.history.push("/score");
+	};
 	render() {
-		var questions = this.props.quiz.questions;
 		return (
 			<>
-				{!this.props.currentUser ? (
-					<h1>Please login or Signup to play the quiz</h1>
-				) : null}
 				{this.props.quiz ? (
 					<>
 						<div className="container mt-4">
 							<h1 className="display-5">{this.props.quiz.title}</h1>
-							<div className="jumbotron mt-6 ml-6 mr-6">
+							<div className="mt-6 ml-6 mr-6">
 								<div className="row">
 									<div className="col mx-auto">
-										{questions.map((question, i) => {
+										{this.props.quiz.questions.map((question, index) => {
 											return (
-												<div className="col-md-6" key={i}>
-													<h4> Q: {question.title}</h4>
+												<div className="col-md-6" key={index}>
+													<h4>Q: {question.title}</h4>
 													<div className="row">
 														{question.options.map((option, i) => {
 															return (
-																<div className="col-6">
+																<div className="col-6" key={i}>
 																	<div className="form-check">
 																		<input
 																			className="form-check-input"
 																			type="checkbox"
-																			value=""
+																			onChange={(e) =>
+																				this.handleCheckbox(
+																					e,
+																					option,
+																					question._id,
+																					index
+																				)
+																			}
 																			id="defaultCheck1"
 																		/>
 																		<label
 																			className="form-check-label"
-																			for="defaultCheck1"
+																			htmlFor="defaultCheck1"
 																		>
 																			{option}
 																		</label>
@@ -55,6 +105,12 @@ class AttemptQuiz extends Component {
 										})}
 									</div>
 								</div>
+								<button
+									className="btn btn-success"
+									onClick={this.handleSubmitQuiz}
+								>
+									Submit
+								</button>
 							</div>
 						</div>
 					</>
@@ -65,12 +121,14 @@ class AttemptQuiz extends Component {
 		);
 	}
 }
-function mapStateToProps({ quiz, currentUser }) {
-	console.log(quiz.quiz);
-	return {
-		quiz: quiz.quiz,
-		currentUser: currentUser.userInfo,
-	};
+function mapStateToProps({ quiz }) {
+	if (quiz.quiz) {
+		var updatedQuestions = quiz.quiz.questions.map((question) => {
+			question.answers = [];
+			return question;
+		});
+		return { quiz: { ...quiz.quiz, questions: updatedQuestions } };
+	}
 }
 
 export default connect(mapStateToProps)(AttemptQuiz);
